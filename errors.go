d419234abd59ddf5c08625212f7139ac12180f04
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-19 12:30:45 -0700

    errors

---
+++ errors.go
@@ -0,0 +1,9 @@
+package md2
+
+import (
+	"github.com/reiver/go-erorr"
+)
+
+const (
+	errNilReceiver = erorr.Error("md2: nil receiver")
+)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/reiver/go-md2
 
 go 1.24.3
+
+require github.com/reiver/go-erorr v0.0.0-20240801233437-8cbde6d1fa3f
